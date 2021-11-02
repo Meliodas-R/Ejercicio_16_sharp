de258691a847d4a38a3e8ee7e644770bbbaa6c25
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-11-02 11:40:32 +0100

    Agregada la función para comprobar cual de los número es mayor o si son iguales y la demostración por pantalla.

--- Ejercicio_16_sharp/Program.cs
+++ Ejercicio_16_sharp/Program.cs
@@ -6,7 +6,25 @@ namespace Ejercicio_16_sharp
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Introduzca el primer número: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduzca el segundo número: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("\n" + numero1 + " es mayor que " + numero2);
+            }
+            else
+            {
+                if (numero1 == numero2)
+                {
+                    Console.WriteLine("\nLos números son iguales");
+                } else
+                {
+                    Console.WriteLine("\n" + numero2 + " es mayor que " + numero1);
+                }
+            }
         }
     }
 }
